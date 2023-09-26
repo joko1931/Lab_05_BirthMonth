79a503a931db6dfb2f6c77bf5a1e9485cd19e991
joko1931 <okoonja@mail.uc.edu>
2023-09-26 16:33:26 -0400

    final commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import java.util.Scanner;
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter your birthday month [1-12]: ");
+
+        if(in.hasNextInt())
+        {
+            int birthMonth = in.nextInt();
+            in.nextLine();
+            System.out.println("Your birthday month is " + birthMonth);
+        }
+        else
+        {
+            String trash = in.nextLine();
+            System.out.println("You inputted incorrect month value " + trash);
+            System.out.println("Please enter correct input");
+        }
     }
 }
